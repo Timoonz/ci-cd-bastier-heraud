3b6470e0b0e41781daad094ee3e895ab3f281bce
Timoonz <timotheeeheraud@gmail.com>
2026-01-31 16:04:16 +0000

    update pour faire les tests avec la librairie axios

--- src/cli/cli-test.test.ts
+++ src/cli/cli-test.test.ts
@@ -1,11 +1,12 @@
 import { describe, beforeEach, afterEach, test } from '@jest/globals';
+import axios from 'axios';
 import { createVehicle, CreateVehicleOptions } from './create-vehicle';
 
 
 // On veut créer un faux serveur HTPP vers lequel tester la fonction createVehicle
-// Comme les fonctions ne sont pas implémentées, on suppose que l'on va utiliser des fetch
-const mockFetch = jest.fn();
-global.fetch = mockFetch;
+// Nous allons utiliser la librairie axios pour faire des appels au serveur, 
+// et donc nous devons mocker le module axios
+jest.mock('axios');
 
 
 
@@ -18,24 +19,16 @@ const testOptions: CreateVehicleOptions = {
   latitude:  30.0,
 };
 
-function fakeResponse(status: number, body: unknown) {
-  return {
-    status,
-    ok: status >= 200 && status < 300,
-    json: jest.fn().mockResolvedValue(body),
-  };
-}
-
 // On réinitialise l'état du mock avant chaque test
 beforeEach(() => {
-  mockFetch.mockReset();
-  jest.spyOn(console, 'log').mockImplementation();   // on capture console.log
-  jest.spyOn(console, 'error').mockImplementation(); // et console.error
+    jest.resetAllMocks();
+    jest.spyOn(console, 'log').mockImplementation();   // on capture console.log
+    jest.spyOn(console, 'error').mockImplementation(); // et console.error
 });
 
 // Après chaque test, on reset les mocks à leurs états originaux
 afterEach(() => {
-  jest.restoreAllMocks();
+    jest.restoreAllMocks();
 });
 
 
@@ -44,27 +37,19 @@ describe("create-vehicle", () => {
 
     test('Requête POST correctement envoyée, et renvoie bien la confirmation', async () => {
 
-        mockFetch.mockResolvedValueOnce(fakeResponse(201, { id: 34 }));
+        (axios.post as jest.Mock).mockResolvedValueOnce({data: { id: 34 }});
 
         await createVehicle(testOptions);
 
-        // On vérifie que l'on a bien 
-        expect(mockFetch).toHaveBeenCalledTimes(1);
-        // On vérifie ce que la fonction a envoyé au serveur
-        expect(mockFetch).toHaveBeenCalledWith(
+        // On vérifie que l'on a bien fait une unique demande au serveur
+        expect(axios.post).toHaveBeenCalledTimes(1);
+        // On vérifie qu'elle a été appelée avec les bonnes valeurs
+        expect(axios.post).toHaveBeenCalledWith(
         'http://localhost:8080/vehicles',
-        expect.objectContaining({
-            method: 'POST',
-            headers: expect.objectContaining({ 'Content-Type': 'application/json' }),
-            body: JSON.stringify({
-            shortcode: 'abcd',
-            battery:   12,
-            longitude: 20.0,
-            latitude:  30.0,
-            }),
-        })
+        { shortcode: 'abcd', battery: 12, longitude: 20.0, latitude: 30.0 }
         );
 
+        // On vérifie que le bon message a été renvoyé à l'utilisateur
         expect(console.log).toHaveBeenCalledWith(
         expect.stringMatching(/Created vehicle `abcd`, with ID `34`/i)
         );
